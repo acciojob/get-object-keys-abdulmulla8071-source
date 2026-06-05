074387d26bafb79f28f8c197ad952b567cf2527b
Abdul Mulla <abdulmulla8071@gmail.com>
2026-06-05 12:19:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 //your JS code here. If required.
-const sutudent = {
-	name: "John",
-};
-function getkey(obj){
-	return Object.key(obj)
+function getkeys(obj) {
+	return Object.keys(obj)
+	
 }
-console.log(getkey(sutudent))
